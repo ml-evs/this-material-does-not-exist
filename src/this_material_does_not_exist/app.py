0defa9eb9c55f1f8b8432591b17b52e2b91829b5
Matthew Evans <git@ml-evs.science>
2023-12-09 22:39:33 +0000

    Linting

--- src/this_material_does_not_exist/app.py
+++ src/this_material_does_not_exist/app.py
@@ -1,21 +1,23 @@
 from __future__ import annotations
 
 import os
-import dash
-import httpx
-import uuid
 import random
-from dash import html
-from dash.dependencies import Input, Output, State
-from pymatgen.core import Lattice, Structure
+import uuid
 
 import crystal_toolkit.components as ctc
+import dash
+import httpx
 from crystal_toolkit.settings import SETTINGS
+from dash import html
+from dash.dependencies import Input, Output, State
 from optimade.adapters.structures import Structure as OptimadeStructure
+from pymatgen.core import Lattice, Structure
 
 app = dash.Dash(
     assets_folder=str(SETTINGS.ASSETS_PATH),
 )
+
+server = app.server
 
 
 # create the Structure object
@@ -36,17 +38,31 @@ layout = html.Div(
         structure_component.title_layout(),
         html.A(None, id="link"),
         structure_component.layout(),
-        html.Div([
-            html.H3("What's your likelihood that this material is synthesizable?"),
-            dash.dcc.Slider(
-                0, 100, 10,
-                value=50,
-                marks={0: "0%", 50: "50%", 100: "100%"},
-                id="slider"
-            ),
-            html.Button("Submit", id="submit", n_clicks=0, style={"margin": "10px"}),
-            html.P("Scores will be recorded anonymously and shared publicly if they prove interesting!"),
-            ], style={"width": "350px", "padding": "20px", "align": "center", "text-align": "center"}),
+        html.Div(
+            [
+                html.H3("What's your likelihood that this material is synthesizable?"),
+                dash.dcc.Slider(
+                    0,
+                    100,
+                    10,
+                    value=50,
+                    marks={0: "0%", 50: "50%", 100: "100%"},
+                    id="slider",
+                ),
+                html.Button(
+                    "Submit", id="submit", n_clicks=0, style={"margin": "10px"}
+                ),
+                html.P(
+                    "Scores will be recorded anonymously and shared publicly if they prove interesting!"
+                ),
+            ],
+            style={
+                "width": "350px",
+                "padding": "20px",
+                "align": "center",
+                "text-align": "center",
+            },
+        ),
         html.Footer(
             [
                 html.Div(
@@ -79,12 +95,11 @@ layout = html.Div(
                     ],
                 ),
             ]
-    ),
+        ),
     ],
     style=dict(
         margin="2em auto", display="grid", placeContent="center", placeItems="center"
     ),
-
 )
 
 # tell crystal toolkit about your app and layout
@@ -93,6 +108,7 @@ ctc.register_crystal_toolkit(app, layout=layout)
 # random string per user session
 session_id = str(uuid.uuid4())
 shuffled_entries = random.sample(range(0, 384938), 1000)
+
 
 @app.callback(
     Output(structure_component.id(), "data"),
@@ -103,25 +119,19 @@ shuffled_entries = random.sample(range(0, 384938), 1000)
     Input(structure_component.id(), "data"),
 )
 def get_structure(value: str, n_clicks: int, data: dict):
-
     results_fname = os.environ.get("RESULTS_PATH", "results.csv")
     if data:
         with open(results_fname, "a") as f:
-            f.write(f'{session_id},{data["properties"]["optimade_id"].split()[1]},{value}\n')
+            f.write(
+                f'{session_id},{data["properties"]["optimade_id"].split()[1]},{value}\n'
+            )
 
     ind = shuffled_entries[n_clicks]
     base_url = "https://optimade-gnome.odbx.science/v1/structures"
     response = httpx.get(f"{base_url}?page_limit=1&page_offset={ind}").json()
     optimade_structure = response["data"][0]
     pmg_structure = OptimadeStructure(optimade_structure).as_pymatgen
-    optimade_id = "GNome "+ optimade_structure["id"].split("/")[-1].split(".")[0]
+    optimade_id = "GNome " + optimade_structure["id"].split("/")[-1].split(".")[0]
     optimade_url = base_url + "/" + optimade_structure["id"]
     pmg_structure.properties["optimade_id"] = optimade_id
     return pmg_structure, optimade_url, optimade_id
-
-
-# run this app with "python path/to/this/file.py"
-# in production, deploy behind gunicorn or similar
-# see Dash docs for more info
-if __name__ == "__main__":
-    app.run(debug=True, port=8050)
